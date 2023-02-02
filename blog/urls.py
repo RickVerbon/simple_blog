7233e1fcb2067cfcf7065e0e36783cff8551bfd8
k0wek <rick89@gmail.com>
2023-02-02 16:00:00 +0100

    added some features, detail view, create view, and showing popular blogs based on views

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from blog.views import BlogListView, BlogItemCreateView, BlogDetailView
 urlpatterns = [
-    path('', BlogListView.as_view(), name="list-view"),
-    path('create/', BlogItemCreateView.as_view(), name="create-view"),
-    path('detail/<int:pk>', BlogDetailView.as_view(), name="detail-view")
+    path('<str:username>/', BlogListView.as_view(), name="list-view"),
+    path('<str:username>/create/', BlogItemCreateView.as_view(), name="create-view"),
+    path('<str:username>/detail/<int:pk>', BlogDetailView.as_view(), name="detail-view")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView, CreateView
@@ -10,6 +11,10 @@ class BlogListView(ListView):
     context_object_name = 'blog_items' #Zodat ik geen 'object_list'  hoef te gebruiken in mijn template
     ordering = ("-date_created",)
 
+    def get_queryset(self):
+        username = self.kwargs.get('username')
+        return BlogItem.objects.filter(author__username=username)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         popular_posts = BlogItem.objects.all().order_by('-views')
@@ -18,7 +23,6 @@ class BlogListView(ListView):
         else:
             context['popular_posts'] = popular_posts
         return context
-
 
 
 class BlogItemCreateView(CreateView):
